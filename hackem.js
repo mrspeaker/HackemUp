7e12f58c53019ebc12bc968047abd3ca006f138d
Mr Speaker <mrspeaker@gmail.com>
2011-07-28 11:49:53 +0200

    Fix for github change to css serving: added mime type

--- hackem.js
+++ hackem.js
@@ -7,7 +7,7 @@
 */
 
 // Loadem Up! 
-window.bookmarklet = (function(opts){fullFunc(opts)})({
+(function(opts){fullFunc(opts)})({
     css : [hnuBase + "hackemup.css"],
     js  : [
         hnuBase + "hackemup.js",
@@ -42,7 +42,7 @@ window.bookmarklet = (function(opts){fullFunc(opts)})({
 // ... http://www.latentmotion.com/downloads/blank-bookmarklet-v1.js
 function fullFunc(a){function d(b){if(b.length===0){a.ready();return false}
 $.getScript(b[0],function(){d(b.slice(1))})}function e(b){$.each(b,function(c,f){$("<link>")
-.attr({href:f,rel:"stylesheet"}).appendTo("head")})}a.jqpath=a.
+.attr({href:f,rel:"stylesheet",type:'text/css'}).appendTo("head")})}a.jqpath=a.
 jqpath||"http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js";
 (function(b){var c=document.createElement("script");c.type="text/javascript";c.src=b;
 c.onload=function(){e(a.css);d(a.js)};document.body.appendChild(c)})(a.jqpath)};
